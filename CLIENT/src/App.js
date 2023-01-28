92459c03b39cfa025d8b706ce6606ba8c63dbde0
Matu36 <matipineda85@live.com.ar>
2023-01-28 01:22:44 -0300

    Mangiare

---
+++ CLIENT/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//import { BrowserRouter, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        MANGIAR-E!!!
+      </h1>
+    </div>
+  );
+}
+
