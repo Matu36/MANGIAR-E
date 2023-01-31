c8c8dfdb9721b0dbc4351dee1b41a844b7c52fc8
gernavarrete <germannavarrete.info@gmail.com>
2023-01-30 22:47:56 -0300

    Estoy probando

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
+import "./App.css";
 //import { BrowserRouter, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        MANGIAR-E!!!
-      </h1>
+      <h1>MANGIAR-E!!! soy german</h1>
     </div>
   );
 }
-
