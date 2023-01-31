f7984e2ab566ad63d2abadf01bbc8afba9e6de5b
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2023-01-30 22:23:29 -0300

    modify-add h2

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
+import "./App.css";
 //import { BrowserRouter, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        MANGIAR-E!!!
-      </h1>
+      <h1>MANGIAR-E!!!</h1>
+      <h2>modificación de Felipe</h2>
     </div>
   );
 }
-
