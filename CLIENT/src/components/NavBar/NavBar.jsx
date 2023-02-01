2acbb25fc2af11c97a35ff0dcd8793d96a29d8cc
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-01 02:47:23 -0300

    added home button on navbar

--- CLIENT/src/components/NavBar/NavBar.jsx
+++ CLIENT/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import s from "./NavBar.module.css";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+ 
+  let navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
   return <div className={s.container}>
-    <h3>MANGIAR-E</h3>
-    <button>HOME</button>
+    <h3 onClick={handleClick}>MANGIAR-E</h3>
+    <button onClick={handleClick}>HOME</button>
   </div>;
 }
 
