dd98d73c6448fc97f9412af512e981289f881ad2
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-01 16:21:33 -0300

    fix navbar

--- CLIENT/src/components/NavBar/NavBar.jsx
+++ CLIENT/src/components/NavBar/NavBar.jsx
@@ -3,17 +3,18 @@ import s from "./NavBar.module.css";
 import { useNavigate } from "react-router-dom";
 
 function NavBar() {
- 
   let navigate = useNavigate();
 
   const handleClick = () => {
-    navigate('/');
+    navigate("/");
   };
 
-  return <div className={s.container}>
-    <h3 onClick={handleClick}>MANGIAR-E</h3>
-    <button onClick={handleClick}>HOME</button>
-  </div>;
+  return (
+    <div className={s.container}>
+      <h3 onClick={handleClick}>MANGIAR-E</h3>
+      <button onClick={handleClick}>HOME</button>
+    </div>
+  );
 }
 
 export default NavBar;
