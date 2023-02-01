2977cb1ef0c41e466590ae482b21b4900a5ae8d7
Felipe Planes <felaupla@MacBook-Pro-de-Felipe.local>
2023-01-31 21:36:58 -0300

    pruebaFelipe

--- CLIENT/src/App.js
+++ CLIENT/src/App.js
@@ -1,16 +1,11 @@
-import './App.css';
-//import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+//import { BrowserRouter, Element } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        MANGIAR-E!!!
-      </h1>
-      <h2>
-        Yamil
-      </h2>
+      <h1>MANGIAR-E!!!</h1>
+      <h2>Yamil</h2>
     </div>
   );
 }
-
