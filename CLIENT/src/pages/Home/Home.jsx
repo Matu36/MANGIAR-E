0b3a11f8b2d786d6191535cc10065cca9154ce21
leo-r37 <leandro_rocha93@hotmail.com>
2023-02-02 14:36:33 -0300

    improved scaffold

--- CLIENT/src/pages/Home/Home.jsx
+++ CLIENT/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
-import SearchBar from "../SearchBar/searchBar";
-import { healthyTips } from "../healthyTips/healthyTips";
+import NavBar from "../../components/NavBar/NavBar";
+import SearchBar from "../../components/SearchBar/searchBar";
+import { healthyTips } from "../../components/healthyTips/healthyTips";
 
 
 
