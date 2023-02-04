9c3fb165a82bfff3fe2f9d4836fbd514d174c5da
YamilLeotta <yamil.leotta@live.com>
2023-02-04 18:07:52 -0300

    add incremental id in post recipe

--- API/src/models/Recipes.js
+++ API/src/models/Recipes.js
@@ -2,10 +2,6 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Recipes', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -31,31 +27,3 @@ module.exports = sequelize => {
     timestamps: false
   });
 };
-
-/*
-[{
-	"id": 782585,
-	"title": "Cannellini Bean and Asparagus Salad with Mushrooms",
-	"image": "https://spoonacular.com/recipeImages/782585-312x231.jpg",
-	"diets": [
-		"gluten free",
-		"dairy free",
-		"lacto ovo vegetarian",
-		"vegan"
-	],
-	"instructions": "Rinse the cannellini beans and soak for 8 hours or overnight in several inches of water.",
-	"ingredients": [
-    {
-      "id": 10716050,
-      "amount": "cannellini beans",
-      "unitShort": "ml",
-      "unitLarge": "mililiters"
-    }, {
-      "id": 14412,
-      "amount": beans",
-      "unitShort": "ml",
-      "unitLarge": "mililiters"
-    }
-  ]
-}]
-*/
