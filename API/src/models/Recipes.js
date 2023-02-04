3ccdb4f686890fc1357461cb8008c8f04208e8d6
YamilLeotta <yamil.leotta@live.com>
2023-02-04 18:11:29 -0300

    fix recipe model

--- API/src/models/Recipes.js
+++ API/src/models/Recipes.js
@@ -2,6 +2,10 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Recipes', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
