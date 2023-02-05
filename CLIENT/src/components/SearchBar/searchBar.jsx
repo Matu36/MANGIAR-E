a8c93970f595422100909413f2b9124555b618a4
gernavarrete <germannavarrete.info@gmail.com>
2023-02-05 12:38:58 -0300

    Css actualizado

--- CLIENT/src/components/SearchBar/searchBar.jsx
+++ CLIENT/src/components/SearchBar/searchBar.jsx
@@ -1,122 +1,129 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setRecipeIdAutocomplete, setSearchValueName } from "../../Redux/actions/index.js";
-import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import {
+  setRecipeIdAutocomplete,
+  setSearchValueName,
+} from "../../Redux/actions/index.js";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 import { filterByDiet, setOrderBy } from "../../Redux/actions/index.js";
-import Select from 'react-select';
-
+import s from "../SearchBar/searchBar.module.css";
+import Select from "react-select";
 
 export default function SearchBar() {
   const dispatch = useDispatch();
-  const recipes =  useSelector((state) => state.recipes);
-  const diets =  useSelector((state) => state.diets);
+  const recipes = useSelector((state) => state.recipes);
+  const diets = useSelector((state) => state.diets);
   const orderBy = useSelector((state) => state.orderBy);
-  const recipeDetailIdAutocomplete = useSelector((state) => state.recipeIdAutocomplete);
+  const recipeDetailIdAutocomplete = useSelector(
+    (state) => state.recipeIdAutocomplete
+  );
 
-  console.log (recipeDetailIdAutocomplete)
+  console.log(recipeDetailIdAutocomplete);
 
   const mapRecipes = recipes.map((r) => {
-    return {name: r.title,
-    id: r.id,
-    img: r.image,
-    diet: r.diets,
-    
-}
-});
+    return { name: r.title, id: r.id, img: r.image, diet: r.diets };
+  });
 
   const handleOnSearch = (string, results) => {
-  console.log(string, results)
-  }
+    console.log(string, results);
+  };
 
   const handleOnHover = (result) => {
-    console.log(result)
-  }
+    console.log(result);
+  };
 
   const handleOnSelect = (item) => {
-    dispatch (setRecipeIdAutocomplete(item.id))
-    console.log(item)
-  }
+    dispatch(setRecipeIdAutocomplete(item.id));
+    console.log(item);
+  };
 
   const handleOnFocus = () => {
-    console.log('Focused')
-  }
+    console.log("Focused");
+  };
 
   const formatResult = (item) => {
     return (
       <>
-        <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+        <span style={{ display: "block", textAlign: "left" }}>{item.name}</span>
       </>
-    )
-  }
+    );
+  };
 
   const handleSearch = (event) => {
     dispatch(setSearchValueName(event.target.value));
   };
 
   const handleFilterbyDiet = (event) => {
-    dispatch(filterByDiet(event.target.value));
+    console.log(event);
+    dispatch(filterByDiet(event.value));
   };
 
   const orderSelectByAlphabetical = [
-    {label: '', value: ''},
-    {label: 'A-Z', value: 'A-Z'},
-    {label: 'Z-A', value: 'Z-A'},
-  ]
+    { label: "Select Order Alphabetical", value: "" },
+    { label: "A-Z", value: "A-Z" },
+    { label: "Z-A", value: "Z-A" },
+  ];
+
+  const optionsDiets = [{ label: "Select Diet", value: " " }].concat(
+    diets.map((diet) => {
+      diet = diet[0].toUpperCase() + diet.slice(1);
+
+      return { label: diet, value: diet };
+    })
+  );
 
   const handleOrder = (e, { type }) => {
-    let cache = {...orderBy};
-    
-    if(orderBy.type !== type) cache.type = type;
-    
+    let cache = { ...orderBy };
+
+    if (orderBy.type !== type) cache.type = type;
+
     cache.order = e.value;
-    
-    dispatch(setOrderBy(cache))
+
+    dispatch(setOrderBy(cache));
   };
 
-return (
-    <nav className="searchNavBar">
-      <div className="sBar">
-
-      <header className="sBar-header">
-        <h2> Buscar Recetas </h2>
-        <div style={{ width: 400 }}>
-        <ReactSearchAutocomplete
-            items = {mapRecipes}
-            onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            autoFocus
-            formatResult={formatResult}
-          />
+  return (
+    <nav className={s.navBarMain}>
+      <div className={s.divContainerMain}>
+        <div className={s.divSearchBar}>
+          <div className={s.divReactSearchAutocomplete}>
+            <ReactSearchAutocomplete
+              items={mapRecipes}
+              onSearch={handleOnSearch}
+              onHover={handleOnHover}
+              onSelect={handleOnSelect}
+              onFocus={handleOnFocus}
+              autoFocus
+              formatResult={formatResult}
+              placeholder="Buscar Recetas"
+            />
           </div>
-        </header>
-    
+          <div className={s.divSelectByDiets}>
+            <Select
+              className={s.SelectByDiets}
+              options={optionsDiets}
+              onChange={(e) => handleFilterbyDiet(e)}
+              placeholder="Order By Diets"
+            />
+          </div>
+          <div className={s.divSelectByAlphabetical}>
+            <Select
+              className={s.SelectByAlphabetical}
+              options={orderSelectByAlphabetical}
+              onChange={(e) => handleOrder(e, { type: "title" })}
+              placeholder="Order By Alphabetical"
+            />
+          </div>
+          <div className={s.divInputName}>
+            <input
+              className={s.inputName}
+              type="text"
+              placeholder="Search by name"
+              onChange={handleSearch}
+            />
+          </div>
         </div>
-        <Select className="selectOrder"
-        options={orderSelectByAlphabetical}
-        onChange={(e) => handleOrder(e, {type: 'title'})}
-      /><br></br>
-   <input className="searchbar" type="text" 
-   placeholder="Search by name" 
-    onChange={handleSearch}/>
-    <h2>Order By:</h2>
-
-<select onChange={handleFilterbyDiet}>
-  <option value=""></option>
-
-  {diets && diets.map((diet, indexkey) => {
-    diet = diet[0].toUpperCase() + diet.slice(1);
-
-    return (
-      <option key={indexkey} value={diet}>
-        {diet}
-      </option>
-    );
-  })}
-</select> <br></br>
-      
-      </nav>
-     
+      </div>
+    </nav>
   );
 }
