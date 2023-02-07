e55722a2e3e098c368dc264b7e1eabd4d1009751
GonzaBolognese <gonza.1703@gmail.com>
2023-02-07 02:39:45 -0300

    getRecipeById done

--- API/src/controllers/getRecipeById.js
+++ API/src/controllers/getRecipeById.js
@@ -1,25 +1,33 @@
 const {
-    Recipes,
-    Recipe_diets,
-  } = require('../db.js');
+  Recipes,
+} = require('../db.js');
 
 module.exports = async (req, res) => {
-    try{
-        const { id } = req.params;
-  
-        Recipes.findOne({
-            where: { id },
-            include: 'Recipe_diets', required: false,
-        }).then(recipe => {
-          if (!recipe) {
-            return res.status(404).send({ message: "recipe not found" });
-          }
-          res.status(200).send(recipe);
-        });
+  try{
 
-    }
-    catch(error) {
-      console.log(error);
-      return res.status(404).send('Error 404');
-    }
+    let recip = await Recipes.findByPk( req.params.id, 
+      {include: ['Recipe_diets', 'Recipe_ingredients'], required: false},
+    );
+    
+    if(!recip){
+      return res.status(404).send('Recipes Not Found');
+    } else {
+      
+      const diets = recip.Recipe_diets.map(diet => diet.diet);
+      const ingredientsId = recip.Recipe_ingredients.map(ingredient => ingredient.ingredientId);
+
+      return res.status(200).send({
+        "id": recip.id,
+        "title": recip.title,
+        "image": recip.image,
+        "diets": diets,
+        "ingredients": ingredientsId,
+      });
+    }   
+
   }
+  catch(error) {
+    console.log(error);
+    return res.status(404).send('Error 404');
+  }
+}
