b33927d31da70c6227b0438d0a3ba081384f98dd
GonzaBolognese <gonza.1703@gmail.com>
2023-02-06 21:28:55 -0300

    getRecipes done

--- API/src/controllers/getRecipes.js
+++ API/src/controllers/getRecipes.js
@@ -5,14 +5,24 @@ const {
 module.exports = async (req, res) => {
     try{
 
-      let Recip = await Recipes.findAll({
+      let recips = await Recipes.findAll({
         include: 'Recipe_diets', required: false,
       });
       
-      if(!Recip.length){
+      if(!recips.length){
         return res.status(404).send('Recipes Not Found');
       } else {
-        return res.send(Recip);
+        let result = [];
+        recips.map(recip => {
+        const diets = recip.Recipe_diets.map(diet => diet.diet);
+          result.push({
+            "id": recip.id,
+            "title": recip.title,
+            "image": recip.image,
+            "diet": diets,
+          })
+        });
+        return res.send(result);
       }   
 
     }
